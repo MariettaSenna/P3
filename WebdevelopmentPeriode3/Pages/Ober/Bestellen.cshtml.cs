d69041f02a82036543c196b58a04be6770d761b3
MariettaSenna <marietta.de.vries@student.nhlstenden.com>
2023-05-25 10:13:57 +0200

    test

---
+++ WebdevelopmentPeriode3/Pages/Ober/Bestellen.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebdevelopmentPeriode3.Repository;
+
+namespace WebdevelopmentPeriode3.Pages.Ober;
+
+public class Bestellen : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+
+    public List<Category> Categories
+    {
+        get { return new CategoryRepository().Get(); }
+    }
+
+
+    public List<Product> Products
+    {
+        get { return new ProductRepository().Get(CategoryId); }
+    }
+
+    [BindProperty(SupportsGet = true)] public int CategoryId { get; set; }
+
+
+}
+
